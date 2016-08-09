a6fe857a27ef1c42a1bdada4df4323c1fed39aa8
AustinSiu <siuaustin13@gmail.com>
2016-08-09 10:44:16 -0500

    Updates

---
+++ main.js
@@ -0,0 +1,10 @@
+require('aframe');
+require('aframe-bmfont-text-component');
+require('aframe-text-component');
+// require('../index.js');
+
+// set event listeners for control
+
+// parse text for content
+// change bmfont-text attr. of page
+// document.getElementById("page").setAttribte("what");
